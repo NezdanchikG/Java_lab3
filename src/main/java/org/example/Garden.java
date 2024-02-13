51a57af6c0bcced18fd66a64621c025604a9d1e9
NezdanchikG <NezdanchikTHANOS@gmail.com>
2024-02-13 22:00:13 +0300

    add test and small changes

---
+++ src/main/java/org/example/Garden.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Garden {
+
+    private int amount = 500;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void getBerry() {
+        amount--;
+    }
+
+}
